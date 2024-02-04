39e0097e1041e5e7ace924b4ccbe1602903b570b
prashantg15 <prashant.gowda@spit.ac.in>
2024-02-04 11:43:40 +0530

    First Commit

---
+++ src/main/java/com/jwtAuthentication/JwtAuthenticationMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jwtAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationMicroServiceApplication.class, args);
+	}
+
+}
